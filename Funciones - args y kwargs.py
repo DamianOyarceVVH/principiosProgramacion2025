d8a238713bf9b450459d578e6ed972ced05be135
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-05-29 15:00:12 -0400

    sussy

---
+++ Funciones - args y kwargs.py
@@ -0,0 +1,64 @@
+print("Funciones: args y kwargs")
+
+print ("\n----------------------------------------------------------------------------------------------")
+print("Función: llevar cosas")
+print ("----------------------------------------------------------------------------------------------\n")
+
+def llevar(*cosas):
+    for cosa in cosas:
+        print(f"- Llevo {cosa}")
+llevar("Lapiz", "Borrador", "Cartulina", "Celular")
+
+print ("\n----------------------------------------------------------------------------------------------")
+print("Función: suma total")
+print ("----------------------------------------------------------------------------------------------\n")
+
+def suma_total(*args):
+    return sum(args)
+print(">", suma_total(3, 5))
+print(">", suma_total(1, 2, 3, 4, 5))
+
+print ("\n----------------------------------------------------------------------------------------------")
+print("Función: sumar todos")
+print ("----------------------------------------------------------------------------------------------\n")
+
+def sumar_todos(*numeros):
+    total = 0
+    for num in numeros:
+        total += num
+    return total
+print(">", sumar_todos(1, 2, 3))
+print(">", sumar_todos(10, 20, 30, 40))
+print(">", sumar_todos())
+
+print ("\n----------------------------------------------------------------------------------------------")
+print("Función: registrar datos")
+print ("----------------------------------------------------------------------------------------------\n")
+
+def registrar(**datos):
+    for clave, valor in datos.items():
+        print(f"- {clave}: {valor}")
+registrar(Nombre = "Max", Edad = 17, Ciudad = "Santiago")
+
+print ("\n----------------------------------------------------------------------------------------------")
+print("Función: saludar")
+print ("----------------------------------------------------------------------------------------------\n")
+
+def saludar(**kwargs):
+    if "nombre" in kwargs:
+        print(f"Hola, {kwargs['nombre']}!")
+    else: print("Hola, desconocido.")
+    if "edad" in kwargs:
+        print(f"Tienes {kwargs['edad']} años.")
+saludar(nombre = "Martín")
+saludar (nombre = "Alonso", edad = 17)
+
+print ("\n----------------------------------------------------------------------------------------------")
+print("Función: mostrar posicionales y nombrados")
+print ("----------------------------------------------------------------------------------------------\n")
+
+def mostrar(*args,**kwargs):
+    print("Posicionales:", args)
+    print("Nombrados:" ,kwargs)
+mostrar(1, 2, 3, nombre = "Yetzibel", edad = 18)
+    
