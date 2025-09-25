cea5323539673b2d1069b06ec65bc6485a1ac8f0
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-09-25 14:07:00 -0300

    small changes

--- Actividad - Parámetros variables args y kwargs.py
+++ Actividad - Parámetros variables args y kwargs.py
@@ -8,12 +8,14 @@ def estudiantes(*estudiantes, **detalles):  # Función que recibe varios estudia
     print("\n=== Estudiantes ===")  # Encabezado para la sección de estudiantes
     for estudiante in estudiantes:  # Recorre cada estudiante recibido
         strip = estudiante.strip()  # Elimina espacios al inicio y al final del nombre
-        print(f"Nombre original: {estudiante}")  # Imprime el nombre tal como fue ingresado
-        print(f"> lower: {strip.lower()}")  # Convierte todo el nombre a minúsculas
-        print(f"> upper: {strip.upper()}")  # Convierte todo el nombre a mayúsculas
-        print(f"> capitalize: {strip.capitalize()}")  # Primera letra mayúscula, el resto minúsculas
-        print(f"> title: {strip.title()}")  # Pone mayúscula en cada palabra
-        print(f"> swapcase: {strip.swapcase()}\n")  # Invierte mayúsculas y minúsculas
+        print(f"Nombre original ({estudiante})")  # Imprime el nombre tal como fue ingresado
+        print(f"> lstrip ({estudiante.lstrip()})") # Elimina espacios al inicio del nombre
+        print(f"> rstrip ({estudiante.rstrip()})") # Elimina espacios al inicio del nombre
+        print(f"> lower ({strip.lower()})")  # Convierte todo el nombre a minúsculas
+        print(f"> upper ({strip.upper()})")  # Convierte todo el nombre a mayúsculas
+        print(f"> capitalize ({strip.capitalize()})")  # Primera letra mayúscula, el resto minúsculas
+        print(f"> title ({strip.title()})")  # Pone mayúscula en cada palabra
+        print(f"> swapcase ({strip.swapcase()})\n")  # Invierte mayúsculas y minúsculas
 
 estudiantes("  ana pérez morales  ",
             "JUAN soto melo",
